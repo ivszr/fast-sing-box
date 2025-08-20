98ca2407537c5ac286135d712d4d6398d368775e
Ivan <81869676+ivszr@users.noreply.github.com>
2025-08-20 17:10:59 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ IFS= read -r input
 wrapped="'$input'"
 
 printf "\033[32;1mInstalling packages\033[0m\n"
-opkg update && opkg install curl kmod-nft-tproxy sing-box jq dnsproxy
+opkg update && opkg install curl kmod-nft-tproxy sing-box jq 
 
 curl -Lo /tmp/gen.sh https://raw.githubusercontent.com/ivszr/fast-sing-box/refs/heads/main/gen.sh
 
@@ -60,56 +60,9 @@ echo "  type filter hook prerouting priority filter; policy accept;" >> /etc/nft
 echo "  meta mark 0x1 meta l4proto { tcp, udp } tproxy ip to 127.0.0.1:12701 counter accept" >> /etc/nftables.d/30-sing-box-tproxy.nft
 echo "}" >> /etc/nftables.d/30-sing-box-tproxy.nft
 
-service dnsmasq stop
 uci set dhcp.@dnsmasq[0].noresolv="1"
-uci set dhcp.@dnsmasq[0].cachesize="10000"
-uci set dhcp.@dnsmasq[0].min_cache_ttl="3600"
-uci set dhcp.@dnsmasq[0].max_cache_ttl="86400"
 uci -q del dhcp.@dnsmasq[0].server
-uci add_list dhcp.@dnsmasq[0].server="127.0.0.1#5354"
-uci add_list dhcp.@dnsmasq[0].server="::1#5354"
-uci commit dhcp
-service dnsmasq start
- 
-# Configure dnsproxy
-uci set dnsproxy.global.enabled="1"
-uci del dnsproxy.global.listen_port
-uci add_list dnsproxy.global.listen_port="5354"
-uci set dnsproxy.cache.enabled="0"  # "1" to enable (Enable this ONLY if you want to test "cache_optimistic" option)
-uci set dnsproxy.cache.cache_optimistic="1"
-uci set dnsproxy.cache.size="2097152"  # Equal to 2 MB (in binary)
-uci del dnsproxy.servers.bootstrap
-uci add_list dnsproxy.servers.bootstrap="8.8.8.8"
-uci add_list dnsproxy.servers.bootstrap="tcp://8.8.8.8"
-uci del dnsproxy.servers.upstream
-uci add_list dnsproxy.servers.upstream="https://8.8.8.8/dns-query"
-uci commit dnsproxy
-
-cat << "EOF" > /etc/sysctl.d/12-buffer-size.conf
-net.core.rmem_max=7500000
-net.core.wmem_max=7500000
-EOF
-sysctl -p /etc/sysctl.d/12-buffer-size.conf
-
-uci del system.ntp.server
-uci add_list system.ntp.server="216.239.35.0"     # time.google.com
-uci add_list system.ntp.server="216.239.35.4"     # time.google.com
-uci add_list system.ntp.server="216.239.35.8"     # time.google.com
-uci add_list system.ntp.server="216.239.35.12"    # time.google.com
-uci add_list system.ntp.server="162.159.200.123"  # time.cloudflare.com
-uci add_list system.ntp.server="162.159.200.1"    # time.cloudflare.com
-uci commit system
-
-# Intercept DNS traffic
-uci -q del firewall.dns_int
-uci set firewall.dns_int="redirect"
-uci set firewall.dns_int.name="Intercept-DNS"
-uci set firewall.dns_int.family="any"
-uci set firewall.dns_int.proto="tcp udp"
-uci set firewall.dns_int.src="lan"
-uci set firewall.dns_int.src_dport="53"
-uci set firewall.dns_int.target="DNAT"
-uci commit firewall
+uci add_list dhcp.@dnsmasq[0].server="127.0.0.1#1053"
 
 sh /tmp/gen.sh "$wrapped" > /etc/sing-box/config.json
 
