dc0aeea14c3c3621a201da08eee102d5cee75462
Ivan <81869676+ivszr@users.noreply.github.com>
2025-08-20 17:42:17 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
+printf "\033[32;1mInstalling packeges\033[0m\n"
+opkg update && opkg install curl kmod-nft-tproxy sing-box jq
+
+curl -Lo /tmp/gen.sh https://raw.githubusercontent.com/ivszr/fast-sing-box/refs/heads/main/gen.sh
+
 printf 'Введите vless:// ссылку (ОБЯЗАТЕЛЬНО REALITY): '
 IFS= read -r input
 
 wrapped="'$input'"
 
-printf "\033[32;1mInstalling packages\033[0m\n"
-opkg update && opkg install curl kmod-nft-tproxy sing-box jq 
-
-curl -Lo /tmp/gen.sh https://raw.githubusercontent.com/ivszr/fast-sing-box/refs/heads/main/gen.sh
-
 printf "\033[32;1mEnabling sing-box service\033[0m\n"
 if grep -q "option enabled '0'" /etc/config/sing-box; then
-    sed -i "s/	option enabled '0'/	option enabled '1'/" /etc/config/sing-box
+    sed -i "s/	option enabled \'0\'/	option enabled \'1\'/" /etc/config/sing-box
 fi
 if grep -q "option user 'sing-box'" /etc/config/sing-box; then
-    sed -i "s/	option user 'sing-box'/	option user 'root'/" /etc/config/sing-box
+    sed -i "s/	option user \'sing-box\'/	option user \'root\'/" /etc/config/sing-box
 fi
 service sing-box enable
 
@@ -35,6 +35,12 @@ uci commit network
 echo "#!/bin/sh" > /etc/hotplug.d/iface/30-tproxy
 echo "ip route add local default dev lo table 100" >> /etc/hotplug.d/iface/30-tproxy
 
+uci set dhcp.@dnsmasq[0].noresolv="1"
+uci -q delete dhcp.@dnsmasq[0].server
+uci add_list dhcp.@dnsmasq[0].server="127.0.0.1#5453"
+uci add_list dhcp.@dnsmasq[0].server='/use-application-dns.net/'
+uci commit dhcp
+
 printf "\033[32;1mConfigure firewall\033[0m\n"
 rule_id2=$(uci show firewall | grep -E '@rule.*name=.Fake IP via proxy.' | awk -F '[][{}]' '{print $2}' | head -n 1)
 if [ ! -z "$rule_id2" ]; then
@@ -43,7 +49,7 @@ fi
 
 uci add firewall rule
 uci set firewall.@rule[-1]=rule
-uci set firewall.@rule[-1].name='Traffic to proxy'
+uci set firewall.@rule[-1].name='Internet via proxy'
 uci set firewall.@rule[-1].src='lan'
 uci set firewall.@rule[-1].dest='*'
 uci set firewall.@rule[-1].src_ip='192.168.0.0/16'
@@ -62,5 +68,7 @@ echo "}" >> /etc/nftables.d/30-sing-box-tproxy.nft
 
 sh /tmp/gen.sh "$wrapped" > /etc/sing-box/config.json
 
-printf "\033[32;1mInstallation done!\033[0m\n"
-service dnsmasq restart && service network restart && service firewall restart && service sing-box restart
+service dnsmasq restart && service network restart && service firewall restart
+service sing-box restart
+
+
